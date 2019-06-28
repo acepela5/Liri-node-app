50ed8404f2ddb6748fe97c2e0614ea0031953ad1
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-06-28 18:28:54 -0500

    files created

---
+++ liri.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+function concert-this(){
+
+}
+
+function spotify-this-song(){
+
+}
+
+function movie-this(){
+
+}
+
+function do-what-it-says(){
+    
+}
